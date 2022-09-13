fe26dc499c90ed7af41de6b7c9c96681c5bdd756
dunghenry <trandungksnb6930082001>
2022-09-13 13:21:48 +0700

    add call user online

--- main.js
+++ main.js
@@ -1,5 +1,58 @@
 const socket = io('http://localhost:3000');
+const remoteId = document.getElementById('remoteId');
+const btnCall = document.getElementById('btnCall');
+const usernameInput = document.getElementById('username');
+const btnRegister = document.getElementById('btnRegister');
+const listUser = document.getElementById('listUser');
+const chat = document.getElementById('chat');
+const register = document.getElementById('register');
+let isRegister = false;
+function render() {
+    if (isRegister) {
+        chat.style.display = 'block';
+        register.style.display = 'none';
+    } else {
+        chat.style.display = 'none';
+        register.style.display = 'block';
+    }
+}
+render();
+socket.on('users_online', (users) => {
+    let html = '';
+    users.forEach((user) => {
+        html += `<li id=${user.id}>${user.username}</li>`;
+    });
+    listUser.innerHTML = html;
+});
 
+socket.on('new_user', (newUser) => {
+    console.log('New User', newUser);
+});
+socket.on('refresh_users_online', (users) => {
+    listUser.innerHTML = '';
+    let html = '';
+    users.forEach((user) => {
+        html += `<li>${user.username}</li>`;
+    });
+    listUser.innerHTML = html;
+});
+
+socket.on('oke', (msg) => {
+    console.log('MSG', msg);
+});
+
+socket.on('register_failure', () => {
+    alert('Username is already taken!');
+});
+socket.on('register_success', (user) => {
+    isRegister = true;
+    render();
+});
+socket.on('user_disconnect', (username) => {
+    if (username) {
+        alert(`${username} disconnected!`);
+    }
+});
 function openStream() {
     const config = { audio: false, video: true };
     return navigator.mediaDevices.getUserMedia(config);
@@ -15,10 +68,11 @@ const peer = new Peer();
 peer.on('open', (id) => {
     const my_peer = document.getElementById('my-peer');
     my_peer.textContent = 'Your ID: ' + id;
+    btnRegister.onclick = function () {
+        const username = usernameInput.value;
+        socket.emit('register', { username, id });
+    };
 });
-
-const remoteId = document.getElementById('remoteId');
-const btnCall = document.getElementById('btnCall');
 
 //Caller
 btnCall.onclick = function () {
@@ -40,4 +94,17 @@ peer.on('call', (call) => {
             playStream('remoteStream', remoteStream),
         );
     });
+});
+
+listUser.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        const id = e.target.id;
+        openStream().then((stream) => {
+            playStream('localStream', stream);
+            const call = peer.call(id, stream);
+            call.on('stream', (remoteStream) =>
+                playStream('remoteStream', remoteStream),
+            );
+        });
+    }
 });
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const io = require('socket.io')(3000, {
+let usersOnline = [];
+const io = require('socket.io')(process.env.PORT || 3000, {
     cors: {
         origin: '*',
     },
@@ -6,4 +7,26 @@ const io = require('socket.io')(3000, {
 
 io.on('connection', (socket) => {
     console.log(socket.id);
+    console.log(usersOnline);
+    socket.on('register', (user) => {
+        const isExist = usersOnline.some((u) => u.username === user.username);
+        if (isExist) {
+            return socket.emit('register_failure');
+        }
+        socket.username = user.username;
+        usersOnline.push(user);
+        // console.log(usersOnline);
+        // socket.emit('users_online', usersOnline);
+        socket.emit('register_success', user);
+        socket.broadcast.emit('new_user', user);
+        io.sockets.emit('users_online', usersOnline);
+        io.emit('oke', 'Hello, world!');
+    });
+    socket.on('disconnect', function () {
+        if (usersOnline.length >= 1) {
+            usersOnline.pop();
+        }
+        io.sockets.emit('refresh_users_online', usersOnline);
+        socket.broadcast.emit('user_disconnect', socket.username);
+    });
 });
